b3b09f65acfd5890e6fbeb873278ad5be60f0158
René <102520473+ShiomeCode@users.noreply.github.com>
2022-03-28 13:19:30 +0200

    AppBase folder

---
+++ App1Final/DecodeClass.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace App1Final
+{
+    public partial class World
+    {
+        [JsonProperty("All")]
+        public All All { get; set; }
+
+        [JsonProperty("Baden-Wurttemberg")]
+        public District BadenWurttemberg { get; set; }
+
+        [JsonProperty("Bayern")]
+        public District Bayern { get; set; }
+
+        [JsonProperty("Berlin")]
+        public District Berlin { get; set; }
+
+        [JsonProperty("Brandenburg")]
+        public District Brandenburg { get; set; }
+
+        [JsonProperty("Bremen")]
+        public District Bremen { get; set; }
+
+        [JsonProperty("Hamburg")]
+        public District Hamburg { get; set; }
+
+        [JsonProperty("Hessen")]
+        public District Hessen { get; set; }
+
+        [JsonProperty("Mecklenburg-Vorpommern")]
+        public District MecklenburgVorpommern { get; set; }
+
+        [JsonProperty("Niedersachsen")]
+        public District Niedersachsen { get; set; }
+
+        [JsonProperty("Nordrhein-Westfalen")]
+        public District NordrheinWestfalen { get; set; }
+
+        [JsonProperty("Rheinland-Pfalz")]
+        public District RheinlandPfalz { get; set; }
+
+        [JsonProperty("Saarland")]
+        public District Saarland { get; set; }
+
+        [JsonProperty("Sachsen")]
+        public District Sachsen { get; set; }
+
+        [JsonProperty("Sachsen-Anhalt")]
+        public District SachsenAnhalt { get; set; }
+
+        [JsonProperty("Schleswig-Holstein")]
+        public District SchleswigHolstein { get; set; }
+
+        [JsonProperty("Thuringen")]
+        public District Thuringen { get; set; }
+
+        [JsonProperty("Unknown")]
+        public District Unknown { get; set; }
+    }
+
+    public partial class All
+    {
+        [JsonProperty("confirmed")]
+        public long Confirmed { get; set; }
+
+        [JsonProperty("recovered")]
+        public long Recovered { get; set; }
+
+        [JsonProperty("deaths")]
+        public long Deaths { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("population")]
+        public long Population { get; set; }
+
+        [JsonProperty("sq_km_area")]
+        public long SqKmArea { get; set; }
+
+        [JsonProperty("life_expectancy")]
+        public string LifeExpectancy { get; set; }
+
+        [JsonProperty("elevation_in_meters")]
+        public long ElevationInMeters { get; set; }
+
+        [JsonProperty("continent")]
+        public string Continent { get; set; }
+
+        [JsonProperty("abbreviation")]
+        public string Abbreviation { get; set; }
+
+        [JsonProperty("location")]
+        public string Location { get; set; }
+
+        [JsonProperty("iso")]
+        public long Iso { get; set; }
+
+        [JsonProperty("capital_city")]
+        public string CapitalCity { get; set; }
+    }
+
+    public partial class District
+    {
+        [JsonProperty("lat")]
+        public string Lat { get; set; }
+
+        [JsonProperty("long")]
+        public string Long { get; set; }
+
+        [JsonProperty("confirmed")]
+        public long Confirmed { get; set; }
+
+        [JsonProperty("recovered")]
+        public long Recovered { get; set; }
+
+        [JsonProperty("deaths")]
+        public long Deaths { get; set; }
+
+        [JsonProperty("updated")]
+        public DateTimeOffset Updated { get; set; }
+    }
+}
+
+
+public partial class World
+{
+    public static World FromJson(string json) => JsonConvert.DeserializeObject<World>(json, Converter.Settings);
+}
+
+public static class Serialize
+{
+    public static string ToJson(this World self) => JsonConvert.SerializeObject(self, Converter.Settings);
+}
+
+internal static class Converter
+{
+    public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+    {
+        MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+        DateParseHandling = DateParseHandling.None,
+        Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+    };
+}
---
+++ App1Final/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace App1Final
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Gl_Confirmed { get; set; }
+        public string Gl_Recovered { get; set; }
+        public string Gl_Deaths { get; set; }
+        public string[] PickerItems { get; set; }
+        public int index { get; set; }
+        World data;
+        public string f = "N0";
+        public MainPage()
+        {
+            // Sets initial data for launch screen
+            SetGlobalStats();
+            SetSearchList();
+
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        // Uses http to fetch from API with a specific criteria
+        // When fetched, the json converter manages data with the custom class World
+        public World Fetch(string specific = "Global")
+        {
+            string url = "https://covid-api.mmediagroup.fr/v1/cases?country=" + specific;
+            using (WebClient client = new WebClient())
+            {
+                string response = client.DownloadString(url);
+
+                return JsonConvert.DeserializeObject<World>(response);
+            }
+        }
+
+        // Launch screen labels are set when fetch is run
+        private void SetGlobalStats()
+        {
+            data = Fetch(specific: "Global");
+            Gl_Confirmed = "Confirmed:  " + data.All.Confirmed.ToString(format: f);
+            Gl_Recovered = "Recovered:  " + data.All.Recovered.ToString(format: f);
+            Gl_Deaths = "Deaths:  " + data.All.Deaths.ToString(format: f);
+            OnPropertyChanged("Gl_Confirmed");
+            OnPropertyChanged("Gl_Recovered");
+            OnPropertyChanged("Gl_Deaths");
+        }
+
+        // Navigates to a new page with the search criteria country for a deeper look
+        private void Search_Clicked(object sender, EventArgs e)
+        {
+            OnPropertyChanged("index");
+            Navigation.PushAsync(new SearchResult(PickerItems[index], f));
+        }
+
+        // List of countries provided for the demo
+        // NOTE 1: The API contains most countries
+        // NOTE 2: Testing showed that China data is unavailable
+        private void SetSearchList()
+        {
+            PickerItems = new string[5];
+            PickerItems[0] = "Denmark";
+            PickerItems[1] = "Germany";
+            PickerItems[2] = "Sweden";
+            PickerItems[3] = "Norway";
+            PickerItems[4] = "Japan";
+            OnPropertyChanged("PickerItems");
+        }
+    }
+}
